db4ac29a701b80856172525050195503b9b477b6
Olga Tiutiunnyk <izachok@gmail.com>
2021-07-02 15:41:46 +0300

    revert changes in task6

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,10 @@
 const inputRef = document.querySelector('#validation-input');
-const requiredLength = Number(
-  document.querySelector('#validation-input').dataset.length,
-);
 
 inputRef.addEventListener('blur', checkEnteredValue);
 
 function checkEnteredValue(event) {
   const target = event.currentTarget;
+  const requiredLength = Number(target.dataset.length);
   if (!isNaN(requiredLength) && target.value.length === requiredLength) {
     target.classList.add('valid');
     target.classList.remove('invalid');
